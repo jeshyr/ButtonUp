ec437cc790a122f157bd77dba6c0e018dc96d4b8
Ricky Buchanan <ricky@notdoneliving.net>
2015-11-24 14:37:51 +1100

    Implemented die recipe expansion, properly I think.

--- Button Up/Die.swift
+++ Button Up/Die.swift
@@ -17,23 +17,103 @@ struct Die : CustomStringConvertible {
     var value: Int = 0
     var subDice = [DieSubDie]() // For twin die
     
+    // Recipe without skills, braces, or option/swing values
+    var coreRecipe: String {
+        do {
+            let regExp = try NSRegularExpression(pattern: "\\((.+)\\)$", options: NSRegularExpressionOptions.CaseInsensitive)
+            let range = NSMakeRange(0, self.recipe.characters.count)
+            let match = regExp.firstMatchInString(self.recipe, options: NSMatchingOptions(), range: range)
+            
+            if (match != nil) {
+                let range = match!.rangeAtIndex(1) // First capture group
+                let nsRecipe = self.recipe as NSString
+                return nsRecipe.substringWithRange(range)
+            } else {
+                print("Die appears to have no core recipe: \(self.recipe)")
+                return ""
+            }
+        } catch {
+            print("Die::coreRecipe failed to create regular expression")
+            return ""
+        }
+    }
+    
+    // Recipe without skills, braces, but WITH chosen option/swing values
+    // Uses ? if no values chosen
+    var expandedCoreRecipe: String {
+        let rawCoreRecipe = self.coreRecipe // The bit inside the braces
+        
+        do {
+            let range = NSMakeRange(0, rawCoreRecipe.characters.count)
+            
+            // If it's just an integer, return it unchanged - most common case
+            // (6) becomes (6)
+            let regExp = try NSRegularExpression(pattern: "^\\d+$", options: NSRegularExpressionOptions.CaseInsensitive)
+            if regExp.numberOfMatchesInString(rawCoreRecipe, options: NSMatchingOptions(), range: range) > 0 {
+                return rawCoreRecipe
+                
+            } else {
+                // If there's a slash, it's an optional and should have a number of sides added, eg:
+                // (20/2) becomes (2/20=2)
+                let regExp = try NSRegularExpression(pattern: "^\\d+/\\d+$", options: NSRegularExpressionOptions.CaseInsensitive)
+                if regExp.numberOfMatchesInString(rawCoreRecipe, options: NSMatchingOptions(), range: range) > 0 {
+                    return "\(rawCoreRecipe)=\(self.sides)"
+                } else {
+                    // If there's a comma and digit(s), it's a plain twin die and we return it unchanged
+                    let regExp = try NSRegularExpression(pattern: "^\\d+,\\d+$", options: NSRegularExpressionOptions.CaseInsensitive)
+                    if regExp.numberOfMatchesInString(rawCoreRecipe, options: NSMatchingOptions(), range: range) > 0 {
+                        return rawCoreRecipe
+                        
+                    } else {
+                        // If there's a single letter, it's a swing die and should have the number of sides after (each) recipe eg:
+                        // X becomes X=6
+                        let regExp = try NSRegularExpression(pattern: "^[RSTUVWXYZ]$", options: NSRegularExpressionOptions.CaseInsensitive)
+                        if regExp.numberOfMatchesInString(rawCoreRecipe, options: NSMatchingOptions(), range: range) > 0 {
+                            return "\(rawCoreRecipe)=\(self.sides)"
+
+                        } else {
+                            // Last option - twin swing die
+                            // (V,V) becomes (V=6,V=6)
+                            
+                            let regExp = try NSRegularExpression(pattern: "^([RSTUVWXYZ]),([RSTUVWXYZ])$", options: NSRegularExpressionOptions.CaseInsensitive)
+                            let match = regExp.firstMatchInString(rawCoreRecipe, options: NSMatchingOptions(), range: range)
+                                
+                            if (match != nil) {
+                                let nsCoreRecipe = rawCoreRecipe as NSString
+
+                                var range = match!.rangeAtIndex(1) // First capture group
+                                let opt1 = nsCoreRecipe.substringWithRange(range)
+                                range = match!.rangeAtIndex(2) // Second capture group
+                                let opt2 = nsCoreRecipe.substringWithRange(range)
+                                let eachSides = self.sides / 2
+                                return "\(opt1)=\(eachSides),\(opt2)=\(eachSides)"
+                            } else {
+                                print("Die::expandedCoreRecipe failed to identify type of die \(rawCoreRecipe) - can't expand")
+                                return rawCoreRecipe
+                            }
+                        }
+                    }
+                }
+            }
+        } catch {
+            print("Die::expandedCoreRecipe failed to create regular expression")
+            return rawCoreRecipe
+        }
+    }
+    
     // This has to be called 'description' for the CustomStringConvertible protocol
     var description : String {
         // Return full recipe suitable for display on game detail page
-        
-        // Skills (abbreviated) go before the braces, eg
-        // B(30) is a beserk dice
-        // If it's just a number return the recipe
-        // (6) becomes (6)
-        // If there's a letter, it's a swing die and should have the number of sides after (each) recipe eg:
-        // X becomes X=6
-        // (V,V) becomes (V=6,V=6)
-        // If there's a slash, it's an optional and should have a number of sides added, eg:
-        // (20/2) becomes (2/20=2)
+        let core = self.expandedCoreRecipe
+        var skillString = ""
+
+        for skill in self.skills {
+            skillString += skill.short
+        }
         
         // if die.properties.contains(DieFlag.Twin)
-        print("Recipe: \(self.recipe), sides: \(self.sides), skills: \(self.skills)")
-        return self.recipe
+        print("Recipe: \(self.recipe), sides: \(self.sides), skills: \(self.skills), coreRecipe: \(self.coreRecipe), expandedCoreRecipe: \(core)")
+        return "\(skillString)(\(core))"
     }
 }
 
